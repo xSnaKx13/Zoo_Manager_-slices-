4d01f53e731a42a7ae12e10d702d9da568430df9
xSnaKx13 <152309157+xSnaKx13@users.noreply.github.com>
2025-07-25 07:21:04 +0000

    zoo-1

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+type animal struct{
+	animalName string
+	animalSpecies string
+}
+func main(){
+	var allZooAnimals []string
+	var lion string
+	
+}
+func addNewAnumal(animal *animal){
+	for{
+	    var err error
+	    fmt.Print("Имя животного: ")
+	    animal.animalName, err = bufio.NewReader(os.Stdin).ReadString('\n')
+	    animal.animalName = strings.TrimSpace(animal.animalName)
+	    if err != nil{
+		    fmt.Println("Введены невалидные данные")
+		}
+		fmt.Print("Введите вид животного (кошачьи/собачьи/попугай...: )")
+		animal.animalSpecies, err = bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil{
+			fmt.Println("Введены невалидные данные")
+		}
+		if err == nil{
+			break
+		}
+	}
+}
+func printAllAnimal(){
+
+}
